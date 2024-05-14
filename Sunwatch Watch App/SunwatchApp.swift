5db18a431e999b9134c25c11dfdeb3aef4958ec6
Safira <safirahum@gmail.com>
2024-05-14 18:03:09 +0700

    Initial Commit

---
+++ Sunwatch Watch App/SunwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunwatchApp.swift
+//  Sunwatch Watch App
+//
+//  Created by Safira Humaira on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Sunwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
